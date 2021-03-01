797a8425d2233341182cdb1d264dd851fbc2bbd9
unknown <weeslleey354@gmail.com>
2021-02-28 21:21:58 -0300

    style page

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>WeslleyFratini</Text>
+        <Image
+          style={{
+            width: 40,
+            height: 40,
+            borderRadius: 20,
+          }}
+          source={{
+            uri: "https://github.com/weslleyfratini.png",
+          }}
+        />
+      </View>
+
+      <Image
+        style={{
+          width: "100%",
+          height: 350,
+          borderRadius: 20,
+        }}
+        source={{
+          uri: "https://placehold.it/150x150",
+        }}
+      />
+
+      <View>
+        <Text>Like</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
