bf2d8c5b574ddb8a0b435d96c1740438dc1e1ad0
unknown <weeslleey354@gmail.com>
2021-02-28 22:04:57 -0300

    style components

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-function InstaGitHubCard({githubUser}) {
+function InstaGitHubCard({ githubUser }) {
+  const [liked, setLikedState] = useState();
   return (
     <View>
-      <View>
-        <Text>{githubUser}</Text>
+      <View
+        style={{ flexDirection: "row", padding: "15", alignItems: "center" }}
+      >
         <Image
           style={{
+            marginRight: "15",
             width: 40,
             height: 40,
             borderRadius: 20,
@@ -17,6 +28,7 @@ function InstaGitHubCard({githubUser}) {
             uri: `https://github.com/${githubUser}.png`,
           }}
         />
+        <Text>{githubUser}</Text>
       </View>
 
       <Image
@@ -25,40 +37,38 @@ function InstaGitHubCard({githubUser}) {
           height: 350,
           borderRadius: 20,
         }}
+        s
         source={{
           uri: `https://github.com/${githubUser}.png`,
         }}
       />
-
-      <Ionicons name="ios-heart" size={24} color="black" />
+      <View
+        style={{ flexDirection: "row", padding: "15", alignItems: "center" }}
+      >
+        <TouchableOpacity onPress={() => setLikedState(!liked)}>
+          {liked && <Ionicons name="ios-heart" size={24} color="red" />}
+          {!liked && (
+            <Ionicons name="ios-heart-emptys" size={24} color="black" />
+          )}
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 export default function App() {
   return (
-    <View> style={styles.container}
-   <ScrollView>
-      {
-      [
-        "WeslleyFratini", 
-        "WeslleyFratini",
-        "WeslleyFratini", 
-        "WeslleyFratini"
-      ].map((githubUser) =>
-      <InstaGitHubCard key={githubUser} githubUser={githubUser}/>
-      )}
-    </ScrollView>
-    </View>
-
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        {[
+          "WeslleyFratini",
+          "WeslleyFratini",
+          "WeslleyFratini",
+          "WeslleyFratini",
+        ].map((githubUser) => (
+          <InstaGitHubCard key={githubUser} githubUser={githubUser} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+}
